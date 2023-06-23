0aa29a6dc7cf314819f284fd29d4822b20b3d65d
mircea bardan <bardan.mircea.calin@gmail.com>
2023-06-23 21:48:26 +0200

    fixed remove button display inside table

--- src/component/FormNewEntry.tsx
+++ src/component/FormNewEntry.tsx
@@ -1,57 +1,65 @@
 import TableEntry from "../model/TableEntry";
 
 interface FormNewEntryProps {
-    table : TableEntry[],
-    setTable: any,
-    id : number, 
-    setId : any,
-    calculateTotal : any
+  table: TableEntry[];
+  setTable: any;
+  id: number;
+  setId: any;
+  calculateTotal: any;
+  setInvalidPriceMessage: any;
 }
 
-export const FormNewEntry : React.FC<FormNewEntryProps> = (props) => {
+export const FormNewEntry: React.FC<FormNewEntryProps> = (props) => {
+  const { table, setTable, id, setId, calculateTotal, setInvalidPriceMessage } =
+    props;
 
-    const {table, setTable, id, setId, calculateTotal} = props
+  function addEntry(e: any) {
+    setInvalidPriceMessage("");
+    e.preventDefault();
 
-    
-    function addEntry(e :any) {
-        e.preventDefault()
-        setId((prevValue : number) => prevValue + 1)
-        const name = e.currentTarget.name.value
-        const description = e.currentTarget.description.value
-        const price = e.currentTarget.price.value
+    const newEntry = {
+      id: id,
+      name: e.currentTarget.name.value,
+      description: e.currentTarget.description.value,
+      price: e.currentTarget.price.value,
+    };
 
-        let updatedTable = [...table, {id :  id, name: name, description : description, price : price}]
- 
-        console.log(updatedTable)
-
-        calculateTotal(updatedTable)
-        setTable(updatedTable)
-        e.target.reset()
+    let updatedTable = [...table, newEntry];
+    if (!isNaN(newEntry.price)) {
+      calculateTotal(updatedTable);
+      setTable(updatedTable);
+    } else {
+      setInvalidPriceMessage("Invalid price. Nothing added");
     }
-    
-    return (
-        <>
-            <form onSubmit={e => addEntry(e)}>
-                <h3>Add New Expense</h3>
-                    <div className="row-container">
-                        <p>Name</p>
-                        <input type="text" name="name"/>
-                    </div>
-                    <div className="row-container">
-                        <p>Description</p>
-                        <input type="text" name="description" />
-                    </div>
-                    <div className="row-container">
-                        <p>Price</p>
-                        <input type="text" name="price"/>
-                    </div>
-                    
-                <div className="btn-submit">
-                    <input type="submit" value="Add Expense"/>
-                </div>
-                
-    
-            </form>
-        </>
-    )
-}
+
+    // increment the id for the next entry
+    setId((prevValue: number) => prevValue + 1);
+
+    console.log(updatedTable);
+    e.target.reset();
+  }
+
+  return (
+    <>
+      <form onSubmit={(e) => addEntry(e)}>
+        <h2>Add New Expense</h2>
+        <div className="row-container">
+          <p>Name</p>
+          <input type="text" name="name" />
+        </div>
+        <div className="row-container">
+          <p>Description</p>
+          <input type="text-area" name="description" />
+        </div>
+        <div className="row-container">
+          <p>Price</p>
+          <input type="text" name="price" />
+        </div>
+
+        <div className="btn-submit">
+          <input type="submit" value="Add Expense" />
+        </div>
+      </form>
+    </>
+  );
+};
--- src/component/Table.tsx
+++ src/component/Table.tsx
@@ -1,94 +1,102 @@
-import React, { useEffect, useState } from "react"
-import TableEntry from "../model/TableEntry"
-import { FormNewEntry } from "./FormNewEntry"
+import React, { useEffect, useState } from "react";
+import TableEntry from "../model/TableEntry";
+import { FormNewEntry } from "./FormNewEntry";
 
+export const Table: React.FC = () => {
+  const [table, setTable] = useState<TableEntry[]>([]);
+  const [id, setId] = useState(3); //  variable to keep and update a unique id (an alternative to using the uuiv4 library)
+  const [invalidPriceMessage, setInvalidPriceMessage] = useState("");
 
-export const Table : React.FC = () => {
+  // useEffect to insert 3 seed values into the table ( happens once, at component mount)
+  useEffect(() => {
+    const initialValues = [
+      {
+        id: 0,
+        name: "lotto ticket",
+        description: "",
+        price: 20,
+      },
+      {
+        id: 1,
+        name: "Beers",
+        description: "Party night",
+        price: -15,
+      },
+      {
+        id: 2,
+        name: "Phone Bill",
+        description: "January",
+        price: -29.99,
+      },
+    ];
+    setTable(initialValues);
+  }, []);
 
-    const [table, setTable] = useState<TableEntry[]>([])
-    const [id, setId] = useState(3)
-    
-    // useEffect to insert 3 seed values into the table ( happens once, at component mount)
-    useEffect(() => {
-        const initialValues = [
-            {
-                id : 0,
-                name : "lotto ticket",
-                description : "",
-                price : 20
-            },
-            {
-                id : 1,
-                name : "Beers",
-                description : "Party night",
-                price : -15
-            },
-            {
-                id : 2,
-                name : "Phone Bill",
-                description : "January",
-                price : -29.99
-            }
-        ]
-        setTable(initialValues)
-    }, [])
+  // function to delete an entry from the table, by id
+  function deleteEntry(id: number) {
+    let tableCopy = [...table];
+    tableCopy = tableCopy.filter((entry) => entry.id !== id);
+    setTable(tableCopy);
+    setInvalidPriceMessage("");
+  }
 
+  // function to calculate the Total
+  function calculateTotal(arr: TableEntry[]) {
+    let total = 0;
 
-    // function to delete an entry from the table, by id
-    function deleteEntry(id : number) {
-        let tableCopy = [...table]
-        tableCopy = tableCopy.filter(entry => entry.id !== id)
-        setTable(tableCopy);
-    }
+    arr.forEach((elem) => {
+      total = parseInt(total.toString()) + parseInt(elem.price.toString());
+    });
+    return total;
+  }
 
-
-    // function to calculate the Total
-    function calculateTotal(arr : TableEntry[]){
-        let total = 0;
-
-        arr.forEach((elem) => {total = parseInt(total.toString()) + parseInt(elem.price.toString())})
-        return total;
-    }
-
-    return(
-        <>
-        <div className="wrapper">
-            <div className="table-container">
-                <h1>My Piggy Bank</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {table.map(entry => { 
-                            return (
-                                <tr key={entry.id}>
-                                    <td>{entry.name}</td>
-                                    <td>{entry.description}</td>
-                                    <td className="last-col">
-                                        {entry.price}
-                                        <button onClick={() => deleteEntry(entry.id)}>Remove</button>
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>    
-                </table>
-                 <p>Total : {calculateTotal(table)} </p>
-            </div>
-            <div className="form-container">
-                <FormNewEntry 
-                    table={table} setTable={setTable} 
-                    id={id} setId={setId}
-                    calculateTotal={calculateTotal}
-                />
-            </div>
+  return (
+    <>
+      <div className="wrapper">
+        <div className="table-container">
+          <h1>My Piggy Bank</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {table.map((entry) => {
+                return (
+                  <tr key={entry.id}>
+                    <td>{entry.name}</td>
+                    <td>{entry.description}</td>
+                    <td>{entry.price}</td>
+                    <td>
+                      <button onClick={() => deleteEntry(entry.id)}>
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="invalid-price-error">{invalidPriceMessage}</div>
+          <h3>
+            <strong>Total : {calculateTotal(table)}</strong>{" "}
+          </h3>
         </div>
-                
-        </>
-    )
-}
+        <div className="form-container">
+          <FormNewEntry
+            table={table}
+            setTable={setTable}
+            id={id}
+            setId={setId}
+            calculateTotal={calculateTotal}
+            setInvalidPriceMessage={setInvalidPriceMessage}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
